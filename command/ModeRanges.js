2ebbec83c779d42ba0dea47abdfdd71225b4cd96
Matthias Hryniszak <padcom@gmail.com>
2021-03-20 02:54:06 +0100

    Added ModeRanges command

---
+++ command/ModeRanges.js
@@ -0,0 +1,30 @@
+import { Request } from '../Request'
+import { Response } from '../Response'
+
+export const MSP_MODE_RANGES = 34
+
+export class ModeRangesRequest extends Request {
+  constructor() {
+    super(MSP_MODE_RANGES)
+  }
+}
+
+export class ModeRangesResponse extends Response {
+  get count() {
+    return this.payload.byteLength / 4
+  }
+
+  get ranges() {
+    const result = []
+    for (let i = 0; i < this.count; i++) {
+      const id = this.getUint8(i * 2)
+      const auxChannelIndex = this.getUint8(i * 2 + 1)
+      const start = 900 + this.getUint8(i * 2 + 2) * 25
+      const end = 900 + this.getUint8(i * 2 + 3) * 25
+      if (start > 900 && end > 900) {
+        result.push({ id, auxChannelIndex, start, end })
+      }
+    }
+    return result
+  }
+}
